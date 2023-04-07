b28945da441c1d0c19575a808b185ed9bcf25ccb
Branimir Makovic <branimir.markovic@ezderm.com>
2023-04-07 14:52:36 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LocalDataStorageManager
+
+A description of this package.
---
+++ Sources/LocalDataStorageManager/LocalDataStorageManager.swift
@@ -0,0 +1,6 @@
+public struct LocalDataStorageManager {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
